14d7cc2b15ef962a66d75a61ef84d6f4fe74f83c
Simón A Azócar <saimn.azocar@gmail.com>
2023-04-06 14:43:59 -0400

    ADD navbar with styles

---
+++ src/components/ResumeButton.jsx
@@ -0,0 +1,9 @@
+const ResumeButton = () => {
+  return (
+    <button className="text-green text-base border-2 border-green mx-4 py-1 px-3 rounded-md sm:py-2 sm:px-4">
+      Resume
+    </button>
+  );
+};
+
+export default ResumeButton
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy': '#0a192f',
+        'green': '#64ffda'
+      },
+    },
   },
   plugins: [],
 }
