8e36c2ab075e559393e3ba31e5324c297917a388
Simón A Azócar <saimn.azocar@gmail.com>
2023-04-09 02:47:31 -0400

    DONE about section styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,20 +16,20 @@ const technologies2 = [
 
 const About = () => {
   return (
-    <section className='flex flex-col container center mx-auto justify-center bg-navy'>
-      <header className='mb-8 w-full flex flex-row items-center justify-center'>
+    <section className='flex flex-col sm:px-4 container center mx-auto mb-24 justify-center bg-navy'>
+      <header className='mb-8 w-full flex flex-row items-center sm:justify-start justify-center'>
         <h2 className='text-2xl px-3 font-semibold text-lightest-slate md:text-3xl'>
           <span className='code font-thin text-xl text-green'>01.</span> About
           Me
         </h2>
-        <hr className='h-px w-36 sm:w-80 bg-gray-200 border-0 dark:bg-gray-700' />
+        <hr className='h-px w-52 sm:w-6/12 bg-gray-200 border-0 dark:bg-gray-700' />
       </header>
       <div className='mx-auto flex lg:flex-row lg:gap-x-20 flex-col justify-center items-center gap-y-10'>
         <section className='text-slate px-6'>
           <p className='my-4 text-lg w-full'>
-            I am a software engineering student at Universidad Católica Andrés
-            Bello. My passion for coding and interest in frontend development
-            led me to become a Junior Frontend Developer, where I have been able
+            I am a software engineering student at <span className='text-green'>Universidad Católica Andrés
+            Bello.</span> My passion for coding and interest in <span className='text-green'>frontend development </span>
+            led me to become a <span className='text-green'>Junior Frontend Developer </span>, where I have been able
             to design and create visually appealing and user-friendly user
             interfaces for various projects.
           </p>
@@ -37,13 +37,13 @@ const About = () => {
           <p className='my-4 text-lg'>
             I love to create UI that not only looks good but also provides a
             great user experience. In this personal portfolio I&apos;m
-            showcasing my creativity and proficiency in frontend development
+            showcasing my <span className='text-green'>creativity and proficiency </span> in frontend development
             through several projects that highlight my skills in modern frontend
             technologies.
           </p>
 
           <p className='my-4 text-lg'>
-            I am always on the lookout for opportunities to improve my skills
+            I am always on the lookout for opportunities to <span className='text-green'>improve my skills </span>
             and knowledge in software engineering, and I hope to make a positive
             impact on the tech industry in the future.
           </p>
@@ -51,7 +51,7 @@ const About = () => {
           <p>
             Here are a few technologies I&apos;ve been working with recently:
           </p>
-          <div className='flex flex-row'>
+          <div className='mt-8 flex justify-center md:justify-start flex-row gap-x-12'>
             <ul>
               {technologies1.map((item) => (
                 <li key={item.name} className='flex flex-row items-center'>
@@ -59,7 +59,7 @@ const About = () => {
                     xmlns='http://www.w3.org/2000/svg'
                     viewBox='0 0 20 20'
                     fill='currentColor'
-                    className='w-5 h-5'
+                    className='w-5 h-5 text-green'
                   >
                     <path
                       fillRule='evenodd'
@@ -78,7 +78,7 @@ const About = () => {
                     xmlns='http://www.w3.org/2000/svg'
                     viewBox='0 0 20 20'
                     fill='currentColor'
-                    className='w-5 h-5'
+                    className='w-5 h-5 text-green'
                   >
                     <path
                       fillRule='evenodd'
