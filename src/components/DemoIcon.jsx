d502397953125cf78b0b8635eb2d29f4012a4269
Simón A Azócar <saimn.azocar@gmail.com>
2023-04-16 12:18:32 -0400

    FIX svg errors

--- src/components/DemoIcon.jsx
+++ src/components/DemoIcon.jsx
@@ -13,10 +13,10 @@ const DemoIcon = ({ demo }) => {
         viewBox='0 0 24 24'
         fill='none'
         stroke='currentColor'
-        stroke-width='2'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-        class='feather feather-external-link'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='feather feather-external-link'
       >
         <title>External Link</title>
         <path d='M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6'></path>
