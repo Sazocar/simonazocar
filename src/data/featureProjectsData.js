73e06d46cbf8dde0a2c1963227849012c9730212
Simón A Azócar <saimn.azocar@gmail.com>
2023-04-16 16:19:47 -0400

    UPDATE project description

--- src/data/featureProjectsData.js
+++ src/data/featureProjectsData.js
@@ -1,25 +1,25 @@
 const featureProjectsData = [
   {
     title: 'Avo Store',
-    description: 'Hi',
+    description: 'Web Application builded with NextJS. This project was part of a Platzi course where I learned the basics of this powerful framework. To power the app, we used Platzi Avo API. I was lucky enough to work with the talented course teacher Jonathan Alvarez, who made a big contribution to the project. I took my time to make changes to the design to make it fully responsive, while I was also learning how to use Semantic UI.',
     image: '/avo-screenshot.jpeg',
-    tags: ['NextJS, Typescript, Semantic UI, React, Platzi Avo API'],
+    tags: ['NextJS, TypeScript, Semantic UI, React, Platzi Avo API'],
     demo: 'https://avo-nextjs-eight.vercel.app/',
     repo: 'https://github.com/Sazocar/avo-nextjs'
   },
   {
     title: 'Corsi App',
-    description: 'Hi',
+    description: 'As part of our academic pursuits, my class and I developed an API to create a mobile application that simulates the need for an educational platform for anyone to subscribe to diverse courses. This was a great opportunity for me to work with both NestJS and Flutter for the first time, and apply the Software Development concepts that we have learned during our studies.',
     image: '/corsi-screenshot.png',
-    tags: ['Dart, Typescript, Flutter, NestJS, Mobile App, API Rest'],
+    tags: ['Dart, TypeScript, Flutter, NestJS, Mobile App, API Rest'],
     demo: 'https://corsi-soa2.onrender.com/docs',
     repo: 'https://github.com/ucab-ravens/movil'
   },
   {
     title: 'Pokedux',
-    description: 'Hi',
+    description: 'Dynamic React application that seamlessly integrates with PokeAPI to render the first 151 Pokémon. The application is designed with multiples functionalities, incorporating sophisticated features such as localStorage and Redux state management to maintain data integrity and enhance overall usability. ',
     image: '/pokedux-screenshot.png',
-    tags: ['React, Redux, Ant Design, Javascript, PokeApi'],
+    tags: ['React, Redux, Ant Design, JavaScript, PokeApi'],
     demo: 'https://pokedux-virid.vercel.app/',
     repo: 'https://github.com/Sazocar/pokedux'
   },
