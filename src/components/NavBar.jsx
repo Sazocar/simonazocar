8018cd2e66338fa6bafc0cd962d902a74879cf4f
Simón A Azócar <saimn.azocar@gmail.com>
2023-05-07 01:15:05 -0400

    UPDATE navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const NavBar = ({ openModal, setOpenModal }) => {
     <Disclosure as='nav' className='p-1 bg-navy'>
       {({ open }) => (
         <>
-          <div className='w-full absolute mx-auto py-4 px-4 md:px-6 lg:px-8 justify-between'>
+          <div className='w-full absolute mx-auto py-4 px-4 md:px-6 lg:px-8 justify-between '>
             <div className='relative flex h-16 items-center justify-between'>
               <div className='absolute inset-y-0 right-0 flex items-center md:hidden'>
                 {/* Mobile menu button*/}
@@ -64,7 +64,7 @@ const NavBar = ({ openModal, setOpenModal }) => {
                       <a
                         key={item.name}
                         href={item.href}
-                        className='code text-slate flex md:justify-center rounded-md px-3 items-center py-2 text-sm text-xs'
+                        className='code text-slate flex md:justify-center hover:text-green transition hover:ease-in duration-300  rounded-md px-3 items-center py-2 text-sm text-xs'
                         aria-current={item.current ? 'page' : undefined}
                       >
                         <span className='code text-xs text-green mr-1'>
@@ -88,19 +88,16 @@ const NavBar = ({ openModal, setOpenModal }) => {
             }
           >
             <div className='2xl:code md:flex md:items-center md:justify-center space-y-1 px-2 pb-3 pt-2'>
-              {navigation.map((item) => (
+              {navigation.map((item, index) => (
                 <Disclosure.Button
                   key={item.name}
                   as='a'
                   href={item.href}
-                  className={classNames(
-                    item.current
-                      ? 'text-white'
-                      : 'md:flex md:items-center md:justify-center text-gray-300 hover:text-green',
-                    'md:flex md:flex-column md:justify-center code block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
+                  className='code text-slate flex  justify-center hover:text-green transition hover:ease-in duration-300  rounded-md px-3 items-center py-2 text-sm text-lg'
                 >
+                  <span className='code text-xs text-green mr-1'>
+                    {index !== 4 ? `0${index + 1}.` : null}
+                  </span>
                   {item.name}
                 </Disclosure.Button>
               ))}
