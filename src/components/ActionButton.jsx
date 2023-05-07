82eef27f17b4f5a45abf69809083d3cabfda07d3
Simón A Azócar <saimn.azocar@gmail.com>
2023-05-07 00:32:06 -0400

    FIX navbar position

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -2,7 +2,7 @@ import Button from './Button'
 
 const ActionButton = ({ text, onClick }) => {
   return (
-    <div className='flex justify-center sm:flex sm:justify-start'>
+    <div className='flex justify-center md:flex md:justify-start'>
       {text === 'Resume' ? (
         <a download href='/files/resume.pdf' target='_blank'>
           <Button text={text} onClick={onClick} />
