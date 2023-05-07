752d75517efaa803effb72c78640087a4b9f7500
Simón A Azócar <saimn.azocar@gmail.com>
2023-05-07 01:50:23 -0400

    ADD formation data

---
+++ src/data/formationData.js
@@ -0,0 +1,69 @@
+const formationData = [ 
+  {
+    id: 0,
+    category: 'UCAB',
+    title: ['Software Engineer'],
+    institution: 'Universidad Católica Andrés Bello',
+    date: ['2018', 'Present'],
+    description: [
+      'Programming languages like Java, JavaScript, Dart, C++, Python, and other popular programming languages.',
+      'Software design and application development.',
+      'Creation and management of databases and database management systems.',
+      'Knowledge of computer networks and communication protocols.',
+      'Knowledge of advanced mathematics, such as linear algebra, calculus, and statistics for data analysis and modeling.',
+      'Knowledge of software engineering and software development methodologies.',
+      'Experience in problem solving and research for the creation of innovative solutions in the field of computing.'
+    ],
+  },
+   {
+    id: 1,
+    category: 'Online',
+    title: [
+      'Frontend con ReactJS',
+      'Full Stack con NextJS',
+      'Curso de Frontend Developer',
+      'Curso Profesional de React y Redux',
+      'Curso de Manipulación del DOM',
+      'NestJS',
+      'Desarrollo Backend con Python y Django',
+      'Fundamentos de Bases de Datos',
+    ],
+    institution: 'Platzi',
+    date: ['2021', 'Present'],
+  },
+  {
+    id: 2,
+    category: 'UCV',
+    title: ['Unfinished Degree in Physics'],
+    institution: 'Universidad Central de Venezuela',
+    date: ['2016', '2018'],
+    description: [
+      'Basic concepts of the principles of physics',
+      'Basic knowledge of advance mathematical calculation',
+    ],
+  },
+  {
+    id: 3,
+    category: 'Others',
+    title: ['Network Course'],
+    institution: 'Keys University',
+    date: ['2018', '2019'],
+    description: [
+      'Basic network knowledge',
+      'Cable connection and router configuration',
+      'Optical fiber',
+      'Basic management of Windows and Windows server',
+    ],
+  },
+  {
+    id: 4,
+    category: 'High School',
+    title: ['High School Diploma in Science'],
+    institution: 'Colegio Dulce Nombre de Jesús',
+    date: ['2005', '2015'],
+    description: [
+      'Learning preparation for entrance to higher studies',
+      'Remarkable knowledge in Algebra, English, Physics, Biology and History',
+    ],
+  },
+]
