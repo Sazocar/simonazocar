f50d8db34777b85240ce686a54290dccda8b6c38
Simón A. Azócar <saimn.azocar@gmail.com>
2024-01-19 20:08:03 -0400

    chore(checkout): :lipstick: update text to white

--- src/containers/SocialsContainer.jsx
+++ src/containers/SocialsContainer.jsx
@@ -14,7 +14,7 @@ export function SocialsContainer() {
       <Link href='mailto:saimn.azocar@gmail.com' target='_blank'>
         <EmailIcon />
       </Link>
-      <span className='absolute -top-4 w-56 left-14 end-0 scale-0 z-10 transition-all rounded bg-cyan-900 p-2 text-slate group-hover:scale-100 whitespace-nowrap'>
+      <span className='absolute -top-4 w-56 left-14 end-0 scale-0 z-10 transition-all rounded bg-cyan-900 p-2 text-slate group-hover:scale-100 whitespace-nowrap text-white'>
            saimn.azocar@gmail.com ✨
           </span>
       </div>
