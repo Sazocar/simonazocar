3de4b7f6662880f775babb278ba6ad162969a0a5
Simón A. Azócar <saimn.azocar@gmail.com>
2025-08-02 22:35:07 -0400

    update experience section

--- src/containers/ExperienceSection.jsx
+++ src/containers/ExperienceSection.jsx
@@ -4,12 +4,12 @@ import ButtonSelector from '@/components/ButtonSelector'
 import FormationDescription from '@/components/FormationDescription'
 import Heading from '@/components/Heading'
 
-const ExperienceSection = () => {
+const EducationSection = () => {
   const [selectedJobIndex, setSelectedJobIndex] = useState(0)
 
   return (
-    <section id='experience' className='sm:px-24 container center pt-2 mx-auto'>
-      <Heading number='02' text='Education and Training' width='w-3/5'/>
+    <section id='education' className='sm:px-24 container center pt-2 mx-auto'>
+      <Heading number='02' text='Education' width='w-3/5'/>
       <section className='max-w-2xl mx-auto mt-16 flex flex-col md:flex-row'>
         <div className='flex flex-row md:flex-col justify-center md:justify-start'>
           {/* Render a button for each formation */}
@@ -32,5 +32,4 @@ const ExperienceSection = () => {
   )
 }
 
-export default ExperienceSection
-
+export default EducationSection
