8ee124165d5e0f571e8ba173aac06459cc37f980
Simón A. Azócar <saimn.azocar@gmail.com>
2025-08-02 22:34:51 -0400

    update get in touch section

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -9,14 +9,14 @@ const GetInTouch = () => {
       className='grid place-content-center text-center h-screen my-auto px-12 sm:w-full md:w-3/5 mx-auto'
     >
       <div className='flex flex-row font-thin text-sm md:text-base code justify-center gap-x-4 text-green items-center'>
-        <span>04. </span>
+        <span>05. </span>
         <h4>What&apos;s Next?</h4>
       </div>
       <h1 className='my-5 text-3xl md:text-6xl font-semibold text-lightest-slate'>
         Get In Touch
       </h1>
       <p className='w-full md:w-4/5 mx-auto text-base sm:text-lg text-slate mb-8'>
-       I'm currently crafting beautiful and user-friendly experiences as a Frontend Developer at <Link className="hover:underline text-green decoration-solid" href="https://www.avilatek.com/" target="_blank">AvilaTek 🚀🏔️</Link>, but I'm always eager to explore new opportunities and connect with like-minded individuals. Whether you have a web app in mind, a UI/UX challenge to discuss, or just want to geek out about the latest frontend trends, I'm all ears!
+       At the moment I'm not looking for any new opportunities. I'm working as Software Engineer at <Link className="hover:underline text-green decoration-solid" href="https://www.venbos.com/" target="_blank">Venbos</Link>, but I'm always eager to explore new opportunities and connect with like-minded individuals. Whether you have a web app in mind, a UI/UX challenge to discuss, or just want to geek out about the latest frontend trends, I'm all ears!
       </p>
 
       <SocialsContainer />
@@ -31,4 +31,3 @@ const GetInTouch = () => {
 }
 
 export default GetInTouch
-
