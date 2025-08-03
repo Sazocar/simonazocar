f29a282f136842018f83ddbcd605dc0f48417115
Simón A. Azócar <saimn.azocar@gmail.com>
2025-08-02 22:35:30 -0400

    add work experience data

---
+++ src/data/workExperience.js
@@ -0,0 +1,33 @@
+const formationData = [ 
+  {
+    id: 0,
+    category: 'Venbos',
+    title: ['Software Engineer'],
+    institution: 'Venbos',
+    date: ['August 2025', 'Present'],
+    description: [
+      'Built and delivered technical solutions according to stakeholder business requirements.',
+      'Create from scratch both admin and client apps for corporate transport.',
+      'Proposed and implemented scalable solutions as a Fullstack Developer.',
+      `Participated in meetings with product owners, stakeholders, and the technical leader to define the best solutions for the client's needs.`
+    ],
+    url: 'https://www.venbos.com/'
+  },
+   {
+    id: 1,
+    category: 'AvilaTek',
+    title: ['Junior Software Engineer'],
+    institution: 'AvilaTek',
+    date: ['July 2023', 'May 2025'],
+    description: [
+      `Deliver high-quality, robust production code for a diverse array of projects for clients including ecommerce, shipping logistic and a financial services and capital management company.`,
+      `Work alongside project manager department and tech leads to execute the development and architecture of technical solutions to fulfill business requirements.`,
+      `Collaborate with designers, project managers, and other engineers to transform creative concepts into production realities for clients and stakeholders.`,
+      `Participated in meetings with product owners, stakeholders, and the technical leader to define the best solutions for the client's needs.`,
+      'Support projects in production and respond to customer needs through tickets using agile methodologies.'
+    ],
+    url: 'https://www.avilatek.com/en'
+  },
+]
+
+export default formationData
